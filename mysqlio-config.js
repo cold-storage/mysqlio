7cc4786c7602f46ef3ec43f49b67e0c434393ac5
John Stein <john@johnandkerri.com>
2018-02-09 15:35:22 -0500

    bin stuff

--- mysqlio-config.js
+++ mysqlio-config.js
@@ -5,10 +5,6 @@
 // We have both source and dest configs because you may be pulling data from one
 // db and pushing to another.
 //
-// Generally this would be located at ../mysqlio-somecfg or some other path
-// outside your project so it doesn't get checked into git. But since this is
-// just a local Docker db, doesn't really matter.
-//
 // Configuration options are documented here:
 // https://github.com/mysqljs/mysql#connection-options
 
